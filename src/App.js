d3ce87df16a5f5e7c1fee83c92d6d24cd9bc6391
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-09-28 14:28:40 +0530

    Collecting data

--- src/App.js
+++ src/App.js
@@ -10,90 +10,76 @@ const resourcesDb = {
       link: "https://www.w3schools.com/html/default.asp",
     },
     {
-      title: "w3school - CSS",
-      link: "https://www.w3schools.com/css/default.asp",
+      title: "neogcamp - Getting started with HTML5",
+      link: "https://www.youtube.com/watch?v=No8qdcVYiQw",
     },
     {
-      title: "w3school - JavaScript",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "traversy media - HTML Crash Course For Absolute Beginners",
+      link: "https://www.youtube.com/watch?v=UB1O30fR-EE",
     },
     {
-      title: "w3school - ",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "Programming with mosh - HTML Crash Course",
+      link: "https://www.youtube.com/watch?v=qz0aGYrrlhU",
+    },
+    {
+      title: "MDN - Learn HTML",
+      link: "https://developer.mozilla.org/en-US/docs/Learn/HTML",
+    },
+    {
+      title: "Tutorialspoint - HTML Tutorial",
+      link: "https://www.tutorialspoint.com/html/index.htm",
     },
   ],
   css: [
     {
-      title: "w3school - HTML",
-      link: "https://www.w3schools.com/html/default.asp",
-    },
-    {
       title: "w3school - CSS",
       link: "https://www.w3schools.com/css/default.asp",
     },
     {
-      title: "w3school - JavaScript",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "neogcamp - Getting started with CSS3",
+      link: "https://www.youtube.com/watch?v=u6_a0d94A1Q",
     },
     {
-      title: "w3school - ",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "MDN - Learn CSS",
+      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     },
   ],
   javaScript: [
     {
-      title: "w3school - HTML",
-      link: "https://www.w3schools.com/html/default.asp",
-    },
-    {
-      title: "w3school - CSS",
-      link: "https://www.w3schools.com/css/default.asp",
-    },
-    {
       title: "w3school - JavaScript",
       link: "https://www.w3schools.com/js/default.asp",
     },
     {
-      title: "w3school - ",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "neogcamp - JavaScript in the browser!",
+      link: "https://www.youtube.com/watch?v=yLZazznWoAs",
     },
   ],
-  python: [
+  reactjs: [
     {
-      title: "w3school - HTML",
-      link: "https://www.w3schools.com/html/default.asp",
+      title: "neogcamp - Introduction to ReactJS library",
+      link: "https://www.youtube.com/watch?v=KUJsaM-hAjs&t=6273s",
     },
     {
-      title: "w3school - CSS",
-      link: "https://www.w3schools.com/css/default.asp",
+      title: "official docs - learn reactjs",
+      link: "https://reactjs.org/",
     },
     {
-      title: "w3school - JavaScript",
-      link: "https://www.w3schools.com/js/default.asp",
-    },
-    {
-      title: "w3school - ",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "w3school - Learn reactJs",
+      link: "https://www.w3schools.com/REACT/DEFAULT.ASP",
     },
   ],
-  'c++': [
+  roadmap: [
     {
-      title: "w3school - HTML",
-      link: "https://www.w3schools.com/html/default.asp",
-    },
-    {
-      title: "w3school - CSS",
-      link: "https://www.w3schools.com/css/default.asp",
-    },
-    {
-      title: "w3school - JavaScript",
-      link: "https://www.w3schools.com/js/default.asp",
-    },
-    {
-      title: "w3school - python",
-      link: "https://www.w3schools.com/js/default.asp",
+      title: "roadmap - Developer Roadmaps",
+      link: "https://roadmap.sh/",
     },
   ],
+  reppository: [
+    {
+      title: "microsoft - Web Development for Beginners",
+      link: "https://github.com/microsoft/Web-Dev-For-Beginners",
+    },
+  ]
 };
 
 const language = Object.keys(resourcesDb);
@@ -113,7 +99,7 @@ function App() {
       <main id="main-section">
         <div className="resources-container">
           <div className="keywords-container">
-            <p className="container-text">Keywords</p>
+            <h5 className="container-text">Keywords</h5>
             {language.map((eachLang) => {
               return (
                 <button
@@ -124,14 +110,28 @@ function App() {
                 </button>
               );
             })}
+            <div className="about-info">
+              <h5 className="container-text">About</h5>
+              <p>
+                You can get all usefull resources.
+              </p>
+            </div>
           </div>
           <div className="links-container">
+            <h5 className="container-text">Resources of {resources}</h5>
             {resourcesDb[resources].map((el) => {
               return (
                 <>
                   <div className="link-box">
-                        <a href="{el.link}"><h3>{el.title}</h3> </a>
-                        <a href="{el.link}"><span><i class="fas fa-link"></i></span> </a>
+                    <a href={el.link}>
+                      <h3>{el.title}</h3>{" "}
+                    </a>
+                    <p> {el.link}</p>
+                    <a href={el.link}>
+                      <span>
+                        <i class="fas fa-link"></i>
+                      </span>{" "}
+                    </a>
                   </div>
                 </>
               );
